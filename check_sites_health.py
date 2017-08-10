2cfbfffaa6880e72fe256fb96adc379336d78a23
Dmitry Plevkov <wwarne@gmail.com>
2017-08-10 15:13:41 +0300

    Add sanitize function because addresses in a file can be sloppy.

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,7 +1,11 @@
 import requests
 
+from urllib.parse import urlparse, urlunparse
+
+
 def load_urls4check(path):
     yield from open(path, mode='r', encoding='utf-8')
+
 
 def is_server_respond_with_200(url):
     try:
@@ -10,8 +14,17 @@ def is_server_respond_with_200(url):
         return False
     return resp.status_code == 200
 
+
 def get_domain_expiration_date(domain_name):
     pass
+
+
+def sanitize_address(url):
+    url = url.rstrip('\n')
+    parse_result = urlparse(url)
+    scheme = parse_result or 'http'
+    address = parse_result.netloc or parse_result.path
+    return urlunparse((scheme, address, '', '', '', ''))
 
 if __name__ == '__main__':
     pass
