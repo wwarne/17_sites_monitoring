2044177d72a6cab4db841301c314d9961671c163
Dmitry Plevkov <wwarne@gmail.com>
2017-08-12 15:14:27 +0300

    Working solution

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,13 +1,34 @@
 import requests
 import whois
-
-from datetime import datetime, timedelta
 from urllib.parse import urlparse, urlunparse
 from dateutil.parser import parse
+from datetime import datetime, timedelta
+from collections import namedtuple
+
+
+Domain = namedtuple('Domain', ['url', 'is_respond_200', 'is_expire', 'expire_date'])
 
 
 def load_urls4check(path):
     yield from open(path, mode='r', encoding='utf-8')
+
+
+def sanitize_address(url):
+    url = url.rstrip('\n')
+    parse_result = urlparse(url)
+    scheme = parse_result.scheme or 'http'
+    address = parse_result.netloc or parse_result.path
+    return urlunparse((scheme, address, '', '', '', ''))
+
+
+def get_domain_expiration_date(domain_name):
+    try:
+        w = whois.whois(domain_name)
+    except whois.parser.PywhoisError:
+        return
+    if isinstance(w.expiration_date, (tuple, list)):
+        return w.expiration_date[0]
+    return w.expiration_date
 
 
 def is_server_respond_with_200(url):
@@ -19,32 +40,56 @@ def is_server_respond_with_200(url):
 
 
 def is_expire_in_month(expire_date):
+    if not expire_date:
+        return
     if isinstance(expire_date, str):
         try:
             expire_date = parse(expire_date)
         except ValueError:
-            return None
+            return
     month_from_now = datetime.now() + timedelta(days=30)
-    return expire_date < month_from_now
+    return expire_date <= month_from_now
 
 
-def fetch_domain_expiration_date(domain_name):
-    try:
-        w = whois.whois(domain_name)
-    except whois.parser.PywhoisError:
-        return None
-    expiration_date = w.expiration_date
-    if isinstance(expiration_date, (tuple, list)):
-        expiration_date = expiration_date[0]
-    return expiration_date
+def collect_domain_info(url):
+    url = sanitize_address(url)
+    expire_date = get_domain_expiration_date(url)
+    return Domain(url=url,
+                  is_respond_200=is_server_respond_with_200(url),
+                  is_expire=is_expire_in_month(expire_date),
+                  expire_date=expire_date)
 
 
-def sanitize_address(url):
-    url = url.rstrip('\n')
-    parse_result = urlparse(url)
-    scheme = parse_result or 'http'
-    address = parse_result.netloc or parse_result.path
-    return urlunparse((scheme, address, '', '', '', ''))
+def bool2human(value):
+    if value is None:
+        return 'N/A'
+    elif value:
+        return 'YES'
+    else:
+        return 'NO'
+
+
+def date2string(value):
+    if value is None:
+        return 'N/A'
+    if not isinstance(value, datetime):
+        return str(value)
+    return value.strftime('%d.%m.%Y %H:%M:%S')
+
+
+def pretty_format(domain_obj):
+    if not isinstance(domain_obj, Domain):
+        return
+    url = domain_obj.url
+    respond = bool2human(domain_obj.is_respond_200)
+    expire = bool2human(domain_obj.is_expire)
+    expire_date = date2string(domain_obj.expire_date)
+    return '{:<30}{:<15}{:<15}{:<15}'.format(url, respond, expire, expire_date)
+
 
 if __name__ == '__main__':
-    pass
+    websites = load_urls4check('test.txt')
+    print('{:<30}{:<15}{:<15}{:<15}'.format('Domain', 'Working', 'Expiring', 'Expire date'))
+    for website in websites:
+        site_info = collect_domain_info(website)
+        print(pretty_format(site_info))
